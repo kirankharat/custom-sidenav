29a278f3f9ab5a53809b07a99d246e6efd0a81c0
kirankharat <kirankharat2@rediffmail.com>
2020-08-06 09:08:32 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,24 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { RightContainerComponent } from './components/right-container/right-container.component';
-import { LeftContainerComponent } from './components/left-container/left-container.component';
-import { MainContainerComponent } from './components/main-container/main-container.component';
-import { LeftNavComponent } from './components/left-nav/left-nav.component';
-import { FooterComponent } from './components/footer-component/footer-component';
-import { MasterContainerComponent } from './components/master-container/master-container.component';
-import { HeaderComponentComponent } from './components/header-component/header-component.component';
-import { NavToggleComponent } from './components/nav-toggle/nav-toggle.component';
-import { NavService } from './services/navService/nav.service';
+// import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// import { RightContainerComponent } from './components/right-container/right-container.component';
+// import { LeftContainerComponent } from './components/left-container/left-container.component';
+// import { MainContainerComponent } from './components/main-container/main-container.component';
+// import { LeftNavComponent } from './components/left-nav/left-nav.component';
+// import { FooterComponent } from './components/footer-component/footer-component';
+// import { MasterContainerComponent } from './components/master-container/master-container.component';
+// import { HeaderComponentComponent } from './components/header-component/header-component.component';
+// import { NavToggleComponent } from './components/nav-toggle/nav-toggle.component';
+import { NavService } from './services/nav-service/nav.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, FooterComponent, 
+  declarations: [ 
+    AppComponent, 
+    HelloComponent, 
+    FooterComponent, 
     HeaderComponentComponent,
     MainContainerComponent,
     LeftContainerComponent,
@@ -28,6 +31,7 @@ import { NavService } from './services/navService/nav.service';
     LeftNavComponent,
     MasterContainerComponent,
     NavToggleComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [NavService]
 })
 export class AppModule { }
