43b706b3cd4b6f192fd18bc966adfcabda9b5862
kirankharat <kirankharat2@rediffmail.com>
2020-08-07 08:13:23 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,14 @@ import { HelloComponent } from './hello.component';
 
 // import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 // import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { RightContainerComponent } from './components/right-container/right-container.component';
-import { LeftContainerComponent } from './components/left-container/left-container.component';
-import { MainContainerComponent } from './components/main-container/main-container.component';
-import { LeftNavComponent } from './components/left-nav/left-nav.component';
-import { FooterComponent } from './components/footer-component/footer-component';
-import { MasterContainerComponent } from './components/master-container/master-container.component';
-import { HeaderComponentComponent } from './components/header-component/header-component.component';
-import { NavToggleComponent } from '/components/nav-toggle/nav-toggle.component';
-
-import { NavToggleComponent } from './components/nav-toggle/nav-toggle.component';
+//import { RightContainerComponent } from './components/right-container/right-container.component';
+//import { LeftContainerComponent } from './components/left-container/left-container.component';
+//import { MainContainerComponent } from './components/main-container/main-container.component';
+//import { LeftNavComponent } from './components/left-nav/left-nav.component';
+//import { FooterComponent } from './components/footer-component/footer-component';
+//import { MasterContainerComponent } from './components/master-container/master-container.component';
+//import { HeaderComponentComponent } from './components/header-component/header-component.component';
+//import { NavToggleComponent } from '/components/nav-toggle/nav-toggle.component';
 import { NavService } from './services/nav-service/nav.service';
 
 @NgModule({
@@ -25,14 +23,14 @@ import { NavService } from './services/nav-service/nav.service';
   declarations: [ 
     AppComponent, 
     HelloComponent, 
-    FooterComponent, 
-    HeaderComponentComponent,
-    MainContainerComponent,
-    LeftContainerComponent,
-    RightContainerComponent,
-    LeftNavComponent,
-    MasterContainerComponent,
-     NavToggleComponent 
+    //FooterComponent, 
+    //HeaderComponentComponent,
+    //MainContainerComponent,
+    //LeftContainerComponent,
+    //RightContainerComponent,
+    //LeftNavComponent,
+    //MasterContainerComponent,
+     //NavToggleComponent 
     ],
   bootstrap: [ AppComponent ],
   providers: [NavService]
